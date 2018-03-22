26d68c53d7b9c7b4ca575d3d45832f27c00f9663
Diego Castro <dfc1210@gmail.com>
2018-03-22 15:51:29 -0300

    test

--- generar.bat
+++ generar.bat
@@ -1,3 +1,6 @@
+### https://stackoverflow.com/questions/4541300/export-only-modified-and-added-files-with-folder-structure-in-git
+
+
 git diff-tree -r --no-commit-id --name-only --diff-filter=ACMRT HEAD~1 HEAD
 
 for /f "usebackq tokens=*" %A in (`git diff-tree -r --no-commit-id --name-only --diff-filter=ACMRT HEAD~1 HEAD`) do echo FA|xcopy "%~fA" "C:\Temp\gitfiles\%A"
